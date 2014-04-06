d20afd9cef2ae9cbb5fef0a6806bc66fa5b82bb8
Cédric Vernou <cedric.vernou@gmail.com>
2014-04-06 11:55:10 +0200

    Add DAL.ChampionReader, BO.Champion, BLL.ChampionManager, TEST.ChampionManagerTest

---
+++ UltimateLolTheorycraft.BLL/ChampionManager.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UltimateLolTheorycraft.BO;
+using UltimateLolTheorycraft.DAL;
+
+namespace UltimateLolTheorycraft.BLL
+{
+    /// <summary>
+    /// Manager of champions
+    /// </summary>
+    public class ChampionManager
+    {
+        #region Path of ressources
+        static string dragonPath = Environment.ExpandEnvironmentVariables
+    (@"%ProgramData%\UltimateLolTheorycraft\dragontail-4.4.3\4.4.3");
+        static string championFolderPath = Path.Combine(dragonPath, @"data\fr_FR\champion");
+        static string championPath = championFolderPath + ".json";
+        #endregion
+
+        /// <summary>
+        /// Collection of champions
+        /// </summary>
+        private IEnumerable<Champion> _champions;
+
+        #region Getter/Setter
+        /// <summary>
+        /// Return a champion from her id.
+        /// </summary>
+        /// <param name="id">champion's id</param>
+        /// <returns>champion</returns>
+        public Champion this[string id]
+        {
+            get
+            {
+                foreach (var champion in _champions)
+                    if (champion.id == id)
+                        return champion;
+                return null;
+            }
+        }
+        #endregion
+
+        /// <summary>
+        /// Default constructor that load all champions.
+        /// </summary>
+        public ChampionManager()
+        {
+            _champions = ChampionReader.Load(championPath);
+        }
+    }
+}
---
+++ UltimateLolTheorycraft.BO/Champion.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UltimateLolTheorycraft.BO
+{
+    /// <summary>
+    /// DAO of champion data
+    /// </summary>
+    public class Champion
+    {
+        public string version { get; set; }
+        public string id { get; set; }
+        public int key { get; set; }
+        public string name { get; set; }
+        public string title { get; set; }
+        public string blurb { get; set; }
+        public Stats stats { get; set; }
+    }
+
+    public class Stats
+    {
+        public double hp { get; set; }
+        public double hpperlevel { get; set; }
+        public double mp { get; set; }
+        public double mpperlevel { get; set; }
+        public double movespeed { get; set; }
+        public double armor { get; set; }
+        public double armorperlevel { get; set; }
+        public double spellblock { get; set; }
+        public double spellblockperlevel { get; set; }
+        public double attackrange { get; set; }
+        public double hpregen { get; set; }
+        public double hpregenperlevel { get; set; }
+        public double mpregen { get; set; }
+        public double mpregenperlevel { get; set; }
+        public double crit { get; set; }
+        public double critperlevel { get; set; }
+        public double attackdamage { get; set; }
+        public double attackdamageperlevel { get; set; }
+        public double attackspeedoffset { get; set; }
+        public double attackspeedperlevel { get; set; }
+    }
+}
